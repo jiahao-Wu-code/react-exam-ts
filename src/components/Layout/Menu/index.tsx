322fd46a239d75712b0d768b4eaac265f75201b2
jiahao.wu <1696764441@qq.com>
2024-01-04 21:01:01 +0800

    fix: 修复菜单选中问题

--- src/components/Layout/Menu/index.tsx
+++ src/components/Layout/Menu/index.tsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
 import { adminMenus } from './config';
-import { useNavigate } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 import { RouterKeys, routersData } from '@/router/config';
 
 const items: MenuProps['items'] = adminMenus;
 
 const MenuContainer: React.FC = () => {
     const [current, setCurrent] = useState('');
+    const location = useLocation();
+    useEffect(() => {
+        const current = location.pathname.split('/')[1] as RouterKeys;
+        if(current){
+            setCurrent(current)
+        }
+    }, [])
     const navigate = useNavigate()
     const onClick: MenuProps['onClick'] = (e) => {
         console.log('click ', e.key);
